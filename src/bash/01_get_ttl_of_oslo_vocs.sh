dc26929c08c4899df7c72813320ea1255a2eff2a
gezever <gezever@skynet.be>
2024-08-10 15:03:42 +0200

    geen rdf

--- src/bash/01_get_ttl_of_oslo_vocs.sh
+++ src/bash/01_get_ttl_of_oslo_vocs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 pushd ../main/resources/be/vlaanderen/data/id/vocabulary
 for i in `curl 'https://data.vlaanderen.be/standaarden/api/_content/query' | jq '..|select(contains("/ns/")?)' | cut -d '"' -f 2 |  sed -e 's;/$;;' | sort -u ` ; do
-   ttl=`echo  ${i} |  sed -e 's;/$;;'`;
    pushd ttl_accept
    curl -H "Accept: text/turtle" -L ${i} > $(basename ${i}).ttl
    popd
