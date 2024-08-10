878b46ddf11c54a7d3935028066984a7203c672a
gezever <gezever@skynet.be>
2024-08-10 15:01:45 +0200

    geen rdf

--- src/bash/01_get_ttl_of_oslo_vocs.sh
+++ src/bash/01_get_ttl_of_oslo_vocs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pushd ../main/resources/be/vlaanderen/data/id/vocabulary
-for i in `curl 'https://data.vlaanderen.be/standaarden/api/_content/query/OLkcA4EmgC.1723132518297.json?_params=%7B%22where%22:%5B%7B%22_extension%22:%22json%22%7D,%7B%22_path%22:%22--REGEX+%2F^%5C%5C%2Fstandaarden%2F%22%7D%5D,%22sort%22:%5B%7B%22_file%22:1,%22$numeric%22:true%7D%5D%7D' | jq '..|select(contains("vlaanderen.be/ns/")?)' | cut -d '"' -f 2 |  sed -e 's;/$;;' | sort -u ` ; do
+for i in `curl 'https://data.vlaanderen.be/standaarden/api/_content/query' | jq '..|select(contains("/ns/")?)' | cut -d '"' -f 2 |  sed -e 's;/$;;' | sort -u ` ; do
    ttl=`echo  ${i} |  sed -e 's;/$;;'`;
    pushd ttl_accept
    curl -H "Accept: text/turtle" -L ${i} > $(basename ${i}).ttl
--- src/bash/03_rdf_available.sh
+++ src/bash/03_rdf_available.sh
@@ -10,8 +10,43 @@ popd
 pushd ../main/resources/be/vlaanderen/data/id/vocabulary/ttl_extension
 for x in $(for i in `file * | grep 'with no line terminators' | cut -d '.' -f 1` ; do  grep $i ../index ; done | sort -u) ; do
   echo 'curl -LO '$x'.ttl'
-  curl -L ${x}.ttl
-  curl -L ${x}.rdf
-  curl -L ${x}.jsonld
+#  curl -L ${x}.ttl
+#  curl -L ${x}.rdf
+#  curl -L ${x}.jsonld
 done
 popd
+
+
+curl -LO https://data.vlaanderen.be/ns/bestuurlijk-sanctieregister.ttl
+curl -LO https://data.vlaanderen.be/ns/cultuur-en-jeugd/infrastructuur.ttl
+curl -LO https://data.vlaanderen.be/ns/DigitaleWatermeter.ttl
+curl -LO https://data.vlaanderen.be/ns/fiets.ttl
+curl -LO https://data.vlaanderen.be/ns/financiele-rapportering.ttl
+curl -LO https://data.vlaanderen.be/ns/financiele-rapportering/taxonomie-dcjm.ttl
+curl -LO https://data.vlaanderen.be/ns/gezondheidstoestand.ttl
+curl -LO https://data.vlaanderen.be/ns/kindfiche.ttl
+curl -LO https://data.vlaanderen.be/ns/loongegevens.ttl
+curl -LO https://data.vlaanderen.be/ns/mobiliteit/dienstregeling-en-planning.ttl
+curl -LO https://data.vlaanderen.be/ns/mobiliteit/trips-en-aanbod.ttl
+curl -LO https://data.vlaanderen.be/ns/mobiliteit/vervoersknooppunten.ttl
+curl -LO https://data.vlaanderen.be/ns/nutsvoorzieningen/index_en.html.ttl
+curl -LO https://data.vlaanderen.be/ns/nutsvoorzieningen/meters.ttl
+curl -LO https://data.vlaanderen.be/ns/openbaardomein.ttl
+curl -LO https://data.vlaanderen.be/ns/openbaardomein/begraafplaats.ttl
+curl -LO https://data.vlaanderen.be/ns/openbaardomein/gebied.ttl
+curl -LO https://data.vlaanderen.be/ns/openbaardomein/infrastructuur.ttl
+curl -LO https://data.vlaanderen.be/ns/openbaardomein/terreindeel.ttl
+curl -LO https://data.vlaanderen.be/ns/openbaardomein/vegetatie.ttl
+curl -LO https://data.vlaanderen.be/ns/openbaardomein/waterdeel.ttl
+curl -LO https://data.vlaanderen.be/ns/overlijdensaangifte.ttl
+curl -LO https://data.vlaanderen.be/ns/vastgoed.ttl
+curl -LO https://data.vlaanderen.be/ns/verkeersmetingen.ttl
+curl -LO https://data.vlaanderen.be/ns/vrachtwagenparkeren.ttl
+curl -LO https://purl.eu/ns/consent.ttl
+curl -LO https://purl.eu/ns/mobility/timetables-and-planning.ttl
+curl -LO https://wegenenverkeer.data.vlaanderen.be/ns/abstracten.ttl
+curl -LO https://wegenenverkeer.data.vlaanderen.be/ns/implementatieelement.ttl
+curl -LO https://wegenenverkeer.data.vlaanderen.be/ns/installatie.ttl
+curl -LO https://wegenenverkeer.data.vlaanderen.be/ns/levenscyclus.ttl
+curl -LO https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel.ttl
+curl -LO https://wegenenverkeer.data.vlaanderen.be/ns/proefenmeting.ttl
