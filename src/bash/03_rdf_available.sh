a43c379b21bdc4d6278154c738a53ab196f37012
gezever <gezever@skynet.be>
2024-08-10 13:56:33 +0200

    geen rdf

--- src/bash/03_rdf_available.sh
+++ src/bash/03_rdf_available.sh
@@ -6,7 +6,7 @@ for x in $(for i in `file * | grep HTML | cut -d '.' -f 1` ; do  grep $i ../inde
   echo 'curl -H "Accept: text/turtle" -L '$x
 done
 popd
-
+#zie https://github.com/Informatievlaanderen/OSLO-Standaarden/issues/140
 pushd ../main/resources/be/vlaanderen/data/id/vocabulary/ttl_extension
 for x in $(for i in `file * | grep 'with no line terminators' | cut -d '.' -f 1` ; do  grep $i ../index ; done | sort -u) ; do
   echo 'curl -LO '$x'.ttl'
