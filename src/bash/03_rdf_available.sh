684d01064e7a2a0724fa54a71a1a373eb45714d4
gezever <gezever@skynet.be>
2024-08-10 13:55:00 +0200

    geen rdf  https://github.com/Informatievlaanderen/OSLO-Standaarden/issues/140

--- src/bash/03_rdf_available.sh
+++ src/bash/03_rdf_available.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 pushd ../main/resources/be/vlaanderen/data/id/vocabulary/ttl_accept
 #html ipv ttl
+#zie https://github.com/Informatievlaanderen/OSLO-Standaarden/issues/139
 for x in $(for i in `file * | grep HTML | cut -d '.' -f 1` ; do  grep $i ../index ; done | sort -u) ; do
   echo 'curl -H "Accept: text/turtle" -L '$x
+done
+popd
+
+pushd ../main/resources/be/vlaanderen/data/id/vocabulary/ttl_extension
+for x in $(for i in `file * | grep 'with no line terminators' | cut -d '.' -f 1` ; do  grep $i ../index ; done | sort -u) ; do
+  echo 'curl -LO '$x'.ttl'
+  curl -L ${x}.ttl
+  curl -L ${x}.rdf
+  curl -L ${x}.jsonld
 done
 popd
